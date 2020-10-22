767d83816d6f5054b0b6fe403b2aa7dd4052ff08
Priya Verma <priya.verma@esecforte.com>
2020-10-22 21:08:02 +0530

    Updates

---
+++ precache-manifest.f78f20dac3b4daa63143445c8ff0034f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61351c903a08e120e058f8a879981247",
+    "url": "/react-pagination/index.html"
+  },
+  {
+    "revision": "c0cc099b9deac8ffcc92",
+    "url": "/react-pagination/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "9998fbb37762e0d9eec4",
+    "url": "/react-pagination/static/css/main.67067320.chunk.css"
+  },
+  {
+    "revision": "c0cc099b9deac8ffcc92",
+    "url": "/react-pagination/static/js/2.5a35e37e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pagination/static/js/2.5a35e37e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9998fbb37762e0d9eec4",
+    "url": "/react-pagination/static/js/main.a48166ee.chunk.js"
+  },
+  {
+    "revision": "1fc9be8af1792f662186",
+    "url": "/react-pagination/static/js/runtime-main.b998a50a.js"
+  }
+]);
